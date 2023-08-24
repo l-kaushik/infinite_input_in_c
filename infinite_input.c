db8e0c8e130748efc73c9cd1848f1e89ddb19a6f
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-08-24 19:56:21 +0530

    basic memory allocation

--- infinite_input.c
+++ infinite_input.c
@@ -1,6 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main()
 {
+    int n = 10; // initial size
+    char *input_str;
+
+    input_str = (char *)malloc(n * sizeof(char));
+
+    scanf("%s",input_str);
+    printf("%s",input_str);
     return 0;
 }
