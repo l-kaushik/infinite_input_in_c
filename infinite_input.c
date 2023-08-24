c7ba98427f3863a5716a65dc7105c2a0c7208c99
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-08-24 20:14:03 +0530

    Reallocating If the condition if false then code inside while loop will reallocate the memory for input_str, Created a function check_null() to prevent error because of memory allocations

--- infinite_input.c
+++ infinite_input.c
@@ -2,22 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
+void check_null(char *ptr)
+{
+    if (ptr == NULL)
+    {
+        printf("Memory not allocated");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main()
 {
     int n = 10; // initial size
-    char *input_str;
+    char *input_str = NULL;
 
     input_str = (char *)malloc(n * sizeof(char));
+    check_null(input_str);
 
     fgets(input_str, n, stdin);
 
-    while(strlen(input_str) == n-1 && input_str[n-2] != '\n'){
-        printf("hi\n");
-        break;
+    // string length checker
+    while (strlen(input_str) == n - 1 && input_str[n - 2] != '\n')
+    {
+        n *= 2;
+
+        input_str = realloc(input_str, n * sizeof(char));
+        check_null(input_str);
+
+        fgets(input_str + n / 2 - 1, n / 2 + 1, stdin);
     }
 
-    printf("%s",input_str);
+    printf("%s", input_str);
 
     return 0;
 }
